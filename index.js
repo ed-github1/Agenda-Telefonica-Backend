ba9186f70198d4712743e28df85de60a05137aad
EdgaR <edghartapia@gmail.com>
2024-06-17 09:09:09 -0600

    Added Mongo.js file

--- index.js
+++ index.js
@@ -36,9 +36,7 @@ let persons = [
   },
 ];
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World</h1>");
-});
+
 
 app.get("/info", (request, response) => {
   const actualDate = new Date().toDateString();
