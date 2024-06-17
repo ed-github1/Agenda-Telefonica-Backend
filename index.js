98272b1bbe91a3c4dbbe35ade201319b069bf077
EdgaR <edghartapia@gmail.com>
2024-06-17 12:54:57 -0600

    Added DB on his own module

--- index.js
+++ index.js
@@ -1,42 +1,21 @@
+require("dotenv").config();
+
 const express = require("express");
 const morgan = require("morgan");
+
 const app = express();
-
+const Person = require("./models/person");
 app.use(express.json());
+app.use(express.static("dist"));
 
-// Exercise 3.7
 app.use(morgan("tiny"));
 
-// Exercise 3.8
 morgan.token("body", (req, res) => JSON.stringify(req.body));
 app.use(
   morgan(":method :url :status :response-time ms - :res[content-length] :body")
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-
+let persons = [];
 
 app.get("/info", (request, response) => {
   const actualDate = new Date().toDateString();
@@ -52,7 +31,9 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 const generateId = () => {
@@ -71,11 +52,11 @@ app.post("/api/persons/", (request, response) => {
     return response.status(400).json({ error: "number missing" });
   }
 
-  const person = {
+  const person =  new Person({
     name: body.name,
     number: body.number,
     id: generateId(),
-  };
+  });
 
   persons.forEach((p) => {
     if (p.name === person.name) {
@@ -83,18 +64,18 @@ app.post("/api/persons/", (request, response) => {
     }
   });
 
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  })
+
   persons = persons.concat(person);
-  response.json(person);
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  Person.findById(request.params.id),then(person => 
+    response.json(persons)
+  )
+   
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -104,7 +85,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
